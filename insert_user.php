a22495b599a1626402334927b2f4efdc622b109b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-08 20:44:04 -0300

    inferno

---
+++ insert_user.php
@@ -0,0 +1,20 @@
+<?php
+
+$nome = $_POST ['nome'];
+$setor = $_POST ['setor'];
+$login = $_POST ['login'];
+$senha = $_POST ['senha'];
+
+echo "$nome";
+echo"<br>";
+echo "$setor";
+echo"<br>";
+echo "$login";
+echo"<br>";
+echo "$senha";
+echo"<br>";
+
+
+print_r($_POST);
+
+?>
