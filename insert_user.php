5d79173ac02138f7a99dec738665a07d663ae1e9
enzo miguel <enzo29620@gmail.com>
2024-04-15 20:39:56 -0300

    aff

--- insert_user.php
+++ insert_user.php
@@ -1,17 +1,23 @@
 <?php
+//Recebendo dados do formulário
 
 $nome = $_POST ['nome'];
 $setor = $_POST ['setor'];
 $login = $_POST ['login'];
 $senha = $_POST ['senha'];
 
-echo "$nome";
-echo"<br>";
-echo "$setor";
-echo"<br>";
-echo "$login";
-echo"<br>";
-echo "$senha";
-echo"<br>";
+//Inserindo dados no banco
+
+include 'conexao.php';
+
+//Dados para inserir os dados
+$insert = "INSERT INTO tb_user VALUES (NULL, '$nome', '$setor', '$login', '$senha' )";
+
+//inserindo os dados no banco de dados utilizando a função mysqli
+$query = mysqli_query($conexao, $insert);
+
+echo"inserido com Sucesso";
+
+
 
 ?>
